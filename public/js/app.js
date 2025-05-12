18e51780e816f43a7b4d6bdbd0c7aa1acc2a47a9
GamalBasuoney <gamalbasuoneygb.gb@gmail.com>
2025-05-13 00:16:48 +0300

    final update

--- public/js/app.js
+++ public/js/app.js
@@ -131,67 +131,81 @@ $(function () {
       interval: 0.1,
       batchMax: 4,
       duration: 6,
-      onEnter: batch => gsap.to(batch, {
-        opacity: 1,
-        y: 0,
-        ease: 'sine',
-        stagger: { each: 0.15, grid: [1, 4] },
-        overwrite: true
-      }),
-      onLeave: batch => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
-      onEnterBack: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
-      onLeaveBack: batch => gsap.set(batch, { opacity: 0, y: 50, overwrite: true })
+      onEnter: (batch) =>
+        gsap.to(batch, {
+          opacity: 1,
+          y: 0,
+          ease: "sine",
+          stagger: { each: 0.15, grid: [1, 4] },
+          overwrite: true,
+        }),
+      onLeave: (batch) =>
+        gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
+      onEnterBack: (batch) =>
+        gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+      onLeaveBack: (batch) =>
+        gsap.set(batch, { opacity: 0, y: 50, overwrite: true }),
     });
   });
 
-  const lenis = new Lenis()
+  const lenis = new Lenis();
   function raf(time) {
-    lenis.raf(time)
-    requestAnimationFrame(raf)
+    lenis.raf(time);
+    requestAnimationFrame(raf);
   }
-  requestAnimationFrame(raf)
+  requestAnimationFrame(raf);
 
   gsap.to("[data-speed]", {
-    y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window),
+    y: (i, el) =>
+      (1 - parseFloat(el.getAttribute("data-speed"))) *
+      ScrollTrigger.maxScroll(window),
     ease: "none",
     scrollTrigger: {
       start: 0,
       end: "max",
       invalidateOnRefresh: true,
-      scrub: 0
-    }
+      scrub: 0,
+    },
   });
 
   const animateInUp = document.querySelectorAll(".animate-in-up");
   animateInUp.forEach((element) => {
-    gsap.fromTo(element, {
-      opacity: 0,
-      y: 65,
-      ease: 'sine',
-    }, {
-      y: 0,
-      opacity: 1,
-      scrollTrigger: {
-        trigger: element,
-        toggleActions: 'play none none reverse',
+    gsap.fromTo(
+      element,
+      {
+        opacity: 0,
+        y: 65,
+        ease: "sine",
+      },
+      {
+        y: 0,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: element,
+          toggleActions: "play none none reverse",
+        },
       }
-    });
+    );
   });
 
   const animateRotation = document.querySelectorAll(".animate-rotation");
   animateRotation.forEach((section) => {
     var value = $(section).data("value");
-    gsap.fromTo(section, {
-      ease: 'sine',
-      rotate: 0,
-    }, {
-      rotate: value,
-      scrollTrigger: {
-        trigger: section,
-        scrub: true,
-        toggleActions: 'play none none reverse',
+    gsap.fromTo(
+      section,
+      {
+        ease: "sine",
+        rotate: 0,
+      },
+      {
+        rotate: value,
+        scrollTrigger: {
+          trigger: section,
+          scrub: true,
+          toggleActions: "play none none reverse",
+        },
       }
-    });
+    );
   });
 
   gsap.set(".animate-card-2", { y: 100, opacity: 0 });
@@ -199,16 +213,19 @@ $(function () {
     interval: 0.1,
     batchMax: 2,
     duration: 6,
-    onEnter: batch => gsap.to(batch, {
-      opacity: 1,
-      y: 0,
-      ease: 'sine',
-      stagger: { each: 0.15, grid: [1, 2] },
-      overwrite: true
-    }),
-    onLeave: batch => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
-    onEnterBack: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
-    onLeaveBack: batch => gsap.set(batch, { opacity: 0, y: 100, overwrite: true })
+    onEnter: (batch) =>
+      gsap.to(batch, {
+        opacity: 1,
+        y: 0,
+        ease: "sine",
+        stagger: { each: 0.15, grid: [1, 2] },
+        overwrite: true,
+      }),
+    onLeave: (batch) => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
+    onEnterBack: (batch) =>
+      gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+    onLeaveBack: (batch) =>
+      gsap.set(batch, { opacity: 0, y: 100, overwrite: true }),
   });
 
   gsap.set(".animate-card-3", { y: 50, opacity: 0 });
@@ -216,16 +233,19 @@ $(function () {
     interval: 0.1,
     batchMax: 3,
     duration: 3,
-    onEnter: batch => gsap.to(batch, {
-      opacity: 1,
-      y: 0,
-      ease: 'sine',
-      stagger: { each: 0.15, grid: [1, 3] },
-      overwrite: true
-    }),
-    onLeave: batch => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
-    onEnterBack: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
-    onLeaveBack: batch => gsap.set(batch, { opacity: 0, y: 50, overwrite: true })
+    onEnter: (batch) =>
+      gsap.to(batch, {
+        opacity: 1,
+        y: 0,
+        ease: "sine",
+        stagger: { each: 0.15, grid: [1, 3] },
+        overwrite: true,
+      }),
+    onLeave: (batch) => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
+    onEnterBack: (batch) =>
+      gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+    onLeaveBack: (batch) =>
+      gsap.set(batch, { opacity: 0, y: 50, overwrite: true }),
   });
 
   gsap.set(".animate-card-5", { y: 50, opacity: 0 });
@@ -233,21 +253,30 @@ $(function () {
     interval: 0.1,
     batchMax: 5,
     delay: 1000,
-    onEnter: batch => gsap.to(batch, {
-      opacity: 1,
-      y: 0,
-      ease: 'sine',
-      stagger: { each: 0.15, grid: [1, 5] },
-      overwrite: true
-    }),
-    onLeave: batch => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
-    onEnterBack: batch => gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
-    onLeaveBack: batch => gsap.set(batch, { opacity: 0, y: 50, overwrite: true })
+    onEnter: (batch) =>
+      gsap.to(batch, {
+        opacity: 1,
+        y: 0,
+        ease: "sine",
+        stagger: { each: 0.15, grid: [1, 5] },
+        overwrite: true,
+      }),
+    onLeave: (batch) => gsap.set(batch, { opacity: 1, y: 0, overwrite: true }),
+    onEnterBack: (batch) =>
+      gsap.to(batch, { opacity: 1, y: 0, stagger: 0.15, overwrite: true }),
+    onLeaveBack: (batch) =>
+      gsap.set(batch, { opacity: 0, y: 50, overwrite: true }),
   });
 
-  ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".animate-card-2", { y: 0, opacity: 1 }));
-  ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".animate-card-3", { y: 0, opacity: 1 }));
-  ScrollTrigger.addEventListener("refreshInit", () => gsap.set(".animate-card-5", { y: 0, opacity: 1 }));
+  ScrollTrigger.addEventListener("refreshInit", () =>
+    gsap.set(".animate-card-2", { y: 0, opacity: 1 })
+  );
+  ScrollTrigger.addEventListener("refreshInit", () =>
+    gsap.set(".animate-card-3", { y: 0, opacity: 1 })
+  );
+  ScrollTrigger.addEventListener("refreshInit", () =>
+    gsap.set(".animate-card-5", { y: 0, opacity: 1 })
+  );
 
   $('a[href*="#"]')
     .not('[href="#"]')
@@ -255,7 +284,7 @@ $(function () {
     .click(function (event) {
       if (
         location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
+          this.pathname.replace(/^\//, "") &&
         location.hostname == this.hostname
       ) {
         var target = $(this.hash);
@@ -285,41 +314,6 @@ $(function () {
     });
 
   const toolsSlider = document.querySelector("tools-slider");
-
-  if (!toolsSlider) {
-    const swiper = new Swiper(".swiper-tools", {
-      spaceBetween: 20,
-      autoplay: {
-        delay: 1500,
-        disableOnInteraction: false,
-      },
-      loop: true,
-      grabCursor: true,
-      loopFillGroupWithBlank: true,
-      breakpoints: {
-        1600: {
-          slidesPerView: 5,
-        },
-        1200: {
-          slidesPerView: 4,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        576: {
-          slidesPerView: 2,
-        },
-        0: {
-          slidesPerView: 2,
-        },
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  }
-
   if (!toolsSlider) {
     const swiper = new Swiper(".swiper-testimonials", {
       slidesPerView: 1,
@@ -413,7 +407,7 @@ $(function () {
     if ($("html").hasClass("chrome")) {
       $.smoothScroll();
     }
-  } catch (err) { }
+  } catch (err) {}
 
   $("img, a").on("dragstart", function (event) {
     event.preventDefault();
@@ -445,8 +439,6 @@ $(function () {
       `<div class="picture" style="background-image: url(${imageUrl})"></div>`
     );
   });
-
-
 });
 
 const themeBtn = document.querySelector(".color-switcher");
